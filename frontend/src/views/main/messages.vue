ca68b1101882faf56c5a678879d95afa1e9c3b6d
Alperen Bayram <imranalperenbayram@gmail.com>
2023-02-03 14:49:07 +0300

    messages search engine

--- frontend/src/views/main/messages.vue
+++ frontend/src/views/main/messages.vue
@@ -1,14 +1,44 @@
 <template>
 <div class="messages_list_general_container">
     <div class="messages_list_search_box">
-        <input id="search_box" type="text" placeholder="Search">
+        <input
+            id="search_box"
+            type="text"
+            placeholder="Search Name, Username or Message"
+            v-model="serach_input"
+            @input="filter_chats">
     </div>
-    <div class="users_list">
-        <div class="user">
-            peyami bayram
-        </div>
-        <div class="user">
-            zeliha bayram
+    <div class="users_list" v-if="filtered_chat_contacts">
+        <div class="user" v-for="chat in filtered_chat_contacts" @click="push_message_page(chat.username)" :key="chat.id">
+            <div class="left">
+                <div class="profile_image_container">
+                    <img class="prifle_image" :src="chat.profile_image">
+                </div>
+            </div>
+            <div class="middle">
+                <div class="middle_top">
+                    <p class="target_name">
+                        {{ chat.name }}
+                    </p>
+                    <p class="target_username">
+                        @{{ chat.username }}
+                    </p>
+                </div>
+                <div class="middle_middle">
+                    <p class="you" v-if="chat.sender_id == main_user.id">You: </p>
+                    <p class="you" v-else>{{ chat.name }}:</p>
+                    <!-- <p class="message_preview">{{ chat.message }}</p> -->
+                    <p class="message_preview">{{ chat_message_length_calculator(chat.message) }}</p>
+                </div>
+                <div class="middle_bottom">
+                    <!--TODO mesaj saatinin gorunumunu ayarla  -->
+                    <p>{{ chat.date }}</p>
+                    <!-- <p>15:37</p> -->
+                </div>
+            </div>
+            <div class="right">
+                <p class="notification">1</p>
+            </div>
         </div>
     </div>
 </div>
@@ -21,14 +51,41 @@ export default {
 
     data() {
         return {
-            chat_contacts: null
+            chat_contacts: null,
+            filtered_chat_contacts: null,
+            serach_input: '',
         }
     },
 
     async beforeCreate() {
-        let chat_contacts = await chat_contacts_request()
-        console.log(chat_contacts)
-    }
+        this.chat_contacts = await chat_contacts_request()
+        this.chat_contacts = this.chat_contacts.response
+        this.filtered_chat_contacts = this.chat_contacts
+    },
+
+    methods: {
+        push_message_page(username) {
+            this.$router.push({name: 'message_page', params:{target_username: username}})
+        },
+
+        chat_message_length_calculator(text) {
+            if(text.length > 50) {
+                text = text.substring(0,50) + '....'
+            }
+            return text
+        },
+
+        filter_chats() {
+            if(this.serach_input == '') {
+                this.filtered_chat_contacts = this.chat_contacts
+            }
+            else {
+                this.filtered_chat_contacts = this.chat_contacts.filter(chat => {
+                    return (chat.username && chat.name && chat.message).toLowerCase().includes(this.serach_input.toLowerCase())
+                })                
+            }
+        }
+    },
 
     
 }
@@ -57,6 +114,7 @@ export default {
     text-indent: 10px;
     outline: none;
     max-width: 350px;
+    margin-bottom: 1em;
 }
 
 ::placeholder {
@@ -67,5 +125,79 @@ export default {
 
 #search_box:focus::placeholder {
   color: transparent;
+}
+
+.user {
+    display: grid;
+    grid-template-columns: 1fr 10fr 1fr;
+    align-items: center;
+    padding: .5em;
+    cursor: pointer;
+}
+
+.user:hover {
+    background-color: var(--itemBackground);
+    transition: .2s;
+}
+
+/* .user {
+    display: flex;
+    flex-direction: row;
+}
+
+.left {
+    flex: 1;
+}
+
+.middle {
+    flex: 4;
+}
+
+.right {
+    flex: 1;
+} */
+
+.notification {
+    display: flex;
+    justify-content: center;
+    background-color: var(--tweetBtnBg);
+}
+
+.prifle_image {
+    width: 50px;
+    height: 50px;
+    border-radius: 50%;
+    border: 1px solid white;
+    margin-right: .6em;
+}
+
+.target_username, .target_name {
+    display: inline;
+}
+
+.target_name { 
+    font-weight: bold;
+}
+
+.target_username {
+    color: var(--bordergray);
+}
+
+.middle_bottom > p {
+    color: var(--bordergray);
+    font-size: .8em;
+}
+
+.you, .message_preview {
+    display: inline;
+}
+
+.you {
+    padding-right: .3em;
+    color: var(--bordergray);
+}
+
+.message_preview {
+    font-weight: 100;
 }
 </style>
